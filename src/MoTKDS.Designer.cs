0d0b7dde8f7477905964918f3206586921bd6e54
trung-kieen <trungkiennk4869@gmail.com>
2024-06-24 22:21:28 +0700

    feat: change query for all account instead of  local branch only

--- src/MoTKDS.Designer.cs
+++ src/MoTKDS.Designer.cs
@@ -2623,7 +2623,7 @@ SELECT SOTK, CMND, SODU, MACN, NGAYMOTK FROM LINK0.NGANHANG.dbo.TaiKhoan WHERE (
             this._commandCollection = new global::System.Data.SqlClient.SqlCommand[1];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
-            this._commandCollection[0].CommandText = "SELECT SOTK, CMND, SODU, MACN, NGAYMOTK FROM LINK0.NGANHANG.dbo.TaiKhoan  WHERE MACN = (SELECT TOP 1 MACN FROM [dbo].[ChiNhanh]) ORDER BY MACN";
+            this._commandCollection[0].CommandText = "SELECT SOTK, CMND, SODU, MACN, NGAYMOTK FROM LINK0.NGANHANG.dbo.TaiKhoan ORDER BY MACN";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
         }
         
